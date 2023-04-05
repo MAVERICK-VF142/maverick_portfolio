ce14d544f98456d5e310963ef52adcd077817620
ASHUTOSH JENA <113919175+MAVERICK-VF142@users.noreply.github.com>
2023-04-05 21:34:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+<div class="terminal">
+  <div class="terminal-header">
+    <button></button>
+    <button></button>
+    <button></button>
+  </div>
+  <div class="terminal-body">
+    <p>$ ls</p>
+    <p>index.html style.css script.js</p>
+    <p>$ mkdir images</p>
+    <p>$ cd images</p>
+    <p>$ wget https://via.placeholder.com/150x150</p>
+  </div>
+  <div class="terminal-input">
+    <input type="text" placeholder="$">
+    <button>Submit</button>
+  </div>
+</div>
